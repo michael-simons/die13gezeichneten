0f3398e112330bf5fa0c8c36ca56ec1a3d4322d4
Michael J. Simons <michael@simons.ac>
2017-09-27 16:42:59 +0200

    Generate Spring Boot Application

---
+++ src/main/kotlin/de/jcvogt/die13gezeichneten/Die13GezeichnetenApplication.kt
@@ -0,0 +1,11 @@
+package de.jcvogt.die13gezeichneten
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class Die13GezeichnetenApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(Die13GezeichnetenApplication::class.java, *args)
+}
